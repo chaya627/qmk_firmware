953086921fd530078e58adc0a85bae7cd87f2bb9
chaya627 <t.chaya627@gmail.com>
2016-08-16 20:46:57 +0900

    Update keymap.c

--- keyboards/ergodox_ez/keymaps/default/keymap.c
+++ keyboards/ergodox_ez/keymaps/default/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,        	KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_HOME,
         KC_CAPS,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,		  KC_X,   KC_C,   KC_V,   KC_B,   KC_END,
-        KC_LCTRL,		KC_GRV,       KC_LALT,KC_NO,  LT(SYMB),
+        KC_LCTRL,		KC_GRV,       KC_LALT,KC_NO,  MO(SYMB),
                                              		  KC_NO,  KC_NO,
                                                               KC_NO,
                                                KC_SPC,KC_DELT,KC_NO,
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_PGUP,   KC_Y,   KC_U,   	KC_I,   KC_O,   KC_P,   	JA_AT,
                         KC_H,   KC_J,   	KC_K,   KC_L,   KC_SCLN,	JA_CLON,
              KC_PGDN,	KC_N,   KC_M,   	KC_COMM,KC_DOT, KC_SLSH,	SFT_T(JA_ENUN),
-                                LT(SYMB),	KC_NO,	KC_RALT,KC_GRV,		KC_RCTRL,
+                                MO(SYMB),	KC_NO,	KC_RALT,KC_GRV,		KC_RCTRL,
              KC_LEFT,KC_RGHT,
              KC_UP,
              KC_DOWN,KC_BSPC, KC_ENT
