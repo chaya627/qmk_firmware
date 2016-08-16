c4324d22bbda99ad7507ab830dbc544771267abe
chaya627 <t.chaya627@gmail.com>
2016-08-16 21:16:13 +0900

    Update keymap.c

--- keyboards/ergodox_ez/keymaps/default/keymap.c
+++ keyboards/ergodox_ez/keymaps/default/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  F24   |   1  |   2  |   3  |   4  |   5  |Insert|           |PrtScr|   6  |   7  |   8  |   9  |   0  |   -    |
+ * |  Grv   |   1  |   2  |   3  |   4  |   5  |Insert|           |PrtScr|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | Home |           | PgUp |   Y  |   U  |   I  |   O  |   P  |   @    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| End  |           | PgDn |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | \/Sft  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Ctrl | Grv  |  Alt |      | ~L1  |                                       | ~L1  |      | Alt  | Grv  | Ctrl |
+ *   | Ctrl | Gui  |  Alt |      | ~L1  |                                       | ~L1  |      | Alt  | Gui  | Ctrl |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       | Left | Right  |
@@ -40,11 +40,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_F24,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_INS,
+        KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_INS,
         KC_TAB,        	KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_HOME,
         KC_CAPS,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,		  KC_X,   KC_C,   KC_V,   KC_B,   KC_END,
-        KC_LCTRL,		KC_GRV,       KC_LALT,KC_NO,  MO(SYMB),
+        KC_LCTRL,		KC_LGUI,      KC_LALT,KC_NO,  MO(SYMB),
                                              		  KC_NO,  KC_NO,
                                                               KC_NO,
                                                KC_SPC,KC_DELT,KC_NO,
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_PGUP,   KC_Y,   KC_U,   	KC_I,   KC_O,   KC_P,   	JA_AT,
                         KC_H,   KC_J,   	KC_K,   KC_L,   KC_SCLN,	JA_CLON,
              KC_PGDN,	KC_N,   KC_M,   	KC_COMM,KC_DOT, KC_SLSH,	SFT_T(JA_ENUN),
-                                MO(SYMB),	KC_NO,	KC_RALT,KC_GRV,		KC_RCTRL,
+                                MO(SYMB),	KC_NO,	KC_RALT,KC_RGUI,	KC_RCTRL,
              KC_LEFT,KC_RGHT,
              KC_UP,
              KC_DOWN,KC_BSPC, KC_ENT
